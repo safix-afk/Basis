08a9effbb6f97ec73431dd999971561dcab58966
safichaudhry <safi.u.ch@gmail.com>
2025-12-03 11:50:12 -0500

    Add TypeScript types for react-plotly.js

---
+++ frontend/react-plotly.d.ts
@@ -0,0 +1,20 @@
+declare module 'react-plotly.js' {
+  import { Component } from 'react';
+  
+  interface PlotParams {
+    data: any[];
+    layout?: any;
+    config?: any;
+    style?: React.CSSProperties;
+    useResizeHandler?: boolean;
+    onInitialized?: (figure: any, graphDiv: HTMLElement) => void;
+    onUpdate?: (figure: any, graphDiv: HTMLElement) => void;
+    onPurge?: (figure: any, graphDiv: HTMLElement) => void;
+    onError?: (err: Error) => void;
+    debug?: boolean;
+    revision?: number;
+  }
+  
+  export default class Plot extends Component<PlotParams> {}
+}
+
